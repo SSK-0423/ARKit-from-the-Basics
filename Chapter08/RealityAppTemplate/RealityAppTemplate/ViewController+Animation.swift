f829f421ab5d8f6f74639febee4e1fab85530844
Kotaro Sasagawa <b1900446@planet.kanazawa-it.ac.jp>
2022-05-26 22:13:03 +0900

    Chapter08 done

---
+++ Chapter08/RealityAppTemplate/RealityAppTemplate/ViewController+Animation.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Animation.swift
+//  RealityAppTemplate
+//
+//  Created by 山本知仁 on 2022/05/26.
+//
+
+import Foundation
---
+++ Chapter08/RealityAppTemplate/RealityAppTemplate/ViewController+Model.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Model.swift
+//  RealityAppTemplate
+//
+//  Created by 山本知仁 on 2022/05/26.
+//
+
+import Foundation
