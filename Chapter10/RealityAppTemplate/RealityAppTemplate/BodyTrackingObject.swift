5b20245c40bb95b0ca047f59faf1df747835a7f6
Kotaro Sasagawa <b1900446@planet.kanazawa-it.ac.jp>
2022-06-01 19:19:46 +0900

    Chapter10 done

---
+++ Chapter10/RealityAppTemplate/RealityAppTemplate/BodyTrackingObject.swift
@@ -0,0 +1,8 @@
+//
+//  BodyTrackingObject.swift
+//  RealityAppTemplate
+//
+//  Created by 山本知仁 on 2022/05/30.
+//
+
+import Foundation
---
+++ Chapter10/RealityAppTemplate/RealityAppTemplate/ViewController+Effect.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Effect.swift
+//  RealityAppTemplate
+//
+//  Created by 山本知仁 on 2022/05/30.
+//
+
+import Foundation
